a3d037e4b634f6496e7ea63222652cf4e3e4af76
leekeunhwan <dlrmsghks7@gmail.com>
2018-07-25 23:07:15 +0900

    react-meteor todo list - studying tutos

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { render } from "react-dom";
+
+import App from "../imports/ui/App.js";
+
+// 페이지가 로드되고 준비될 때 코드를 호출하는 방법을 알고 있다.
+// 이 코드는 다른 구성 요소를 로드하고 렌더링 대상 html 요소로 렌더링한다.
+Meteor.startup(() => {
+  render(<App />, document.getElementById("root"));
+});
+
